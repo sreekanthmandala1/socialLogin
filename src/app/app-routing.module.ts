98ee8e67312a6a3fbc7095447b8441fdacd74812
Sreekanth Mandala <smandala@ariqt.com>
2022-08-30 15:10:12 +0530

    added @abacritt/angularx-social-login: ^1.2.4,

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FacebookLoginComponent } from './components/facebook-login/facebook-login.component';
+import { GoogleLoginComponent } from './components/google-login/google-login.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'google', component:GoogleLoginComponent},
+  {path:'facebook', component:FacebookLoginComponent},
+  {path:'',redirectTo:'home',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GoogleLoginComponent } from './components/google-login/google-login.component';
+import { FacebookLoginComponent } from './components/facebook-login/facebook-login.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoogleLoginComponent,
+    FacebookLoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
